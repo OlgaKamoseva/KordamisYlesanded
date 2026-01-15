26e7b6a2f7b416c0fe7a6420bdbb814336e2e872
OlgaKamoseva <olga.kamoseva@tthkee.onmicrosoft.com>
2026-01-15 17:35:34 +0200

    1 ylesanne tehtud
    osaliselt tho, vooskeem veel lopetamata

--- OMISTAMISE_ANDMETÜÜBID_SISEND_VÄLJUND_MUUTUJA/MATH & INPUT/Mathinput1/Program.cs
+++ OMISTAMISE_ANDMETÜÜBID_SISEND_VÄLJUND_MUUTUJA/MATH & INPUT/Mathinput1/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace Mathinput1
+﻿using System.ComponentModel.Design;
+
+namespace Mathinput1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+			//1. kirjuta programm, mis
+			//omab täisarvu muutujat vanuse jaoks
+			//küsib kasutajalt tema vanust. int.Parse() üritab muuta andmeid tekstist arvuks
+			//omab täisarvu muutujat aastaarvu jaoks
+			//küsib kasutajalt praegust kalendriaastat. int.Parse() üritab muuta andmeid tekstist arvuks
+			//omab muutujat sünniaasta jaoks, siia sisse omimstatakse tehte tulemus, kus aastaarvust lahutatakse vanus
+
+			/*     L E T' S   S T A R T   */
+
+			int vanus = 0;
+			Console.WriteLine("Kui vana sa oled?");
+			vanus = int.Parse(Console.ReadLine());
+			int aasta = 0;
+			Console.WriteLine("Mis aasta hetkel on?");
+			aasta = int.Parse(Console.ReadLine());
+			Console.WriteLine("Kas sul juba oli sunnipaev sel aastal? (jah/ei)");
+			string vastus = Console.ReadLine(); 
+			int synniaasta = aasta - vanus;
+			if (vastus == "jah")
+			{
+
+				Console.WriteLine($"Ahhaa, oled sündinud {synniaasta}'l aastal! ");
+			}
+			else
+			{
+				Console.WriteLine($"Ahhaa, oled sündinud {synniaasta - 1}'l aastal! ");
+			}
+		}
+	}
 }
