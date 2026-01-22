0531f5d1e96edc74dcf67836571a9b7e1f52185b
OlgaKamoseva <olga.kamoseva@tthkee.onmicrosoft.com>
2026-01-22 17:32:10 +0200

    Upupupuuu

--- OMISTAMISE_ANDMETÜÜBID_SISEND_VÄLJUND_MUUTUJA/MATH & INPUT/Mathinput1/Program.cs
+++ OMISTAMISE_ANDMETÜÜBID_SISEND_VÄLJUND_MUUTUJA/MATH & INPUT/Mathinput1/Program.cs
@@ -6,36 +6,79 @@ namespace Mathinput1
     {
         static void Main(string[] args)
         {
-			//1. kirjuta programm, mis
-			//omab täisarvu muutujat vanuse jaoks
-			//küsib kasutajalt tema vanust. int.Parse() üritab muuta andmeid tekstist arvuks
-			//omab täisarvu muutujat aastaarvu jaoks
-			//küsib kasutajalt praegust kalendriaastat. int.Parse() üritab muuta andmeid tekstist arvuks
-			//omab muutujat sünniaasta jaoks, siia sisse omimstatakse tehte tulemus, kus aastaarvust lahutatakse vanus
 
 			/*     L E T' S   S T A R T   */
 
-			int vanus = 0;
-			Console.WriteLine("Kui vana sa oled?");
-			vanus = int.Parse(Console.ReadLine());
-			int aasta = 0;
-			Console.WriteLine("Mis aasta hetkel on?");
-			aasta = int.Parse(Console.ReadLine());
-			int synniaasta = aasta - vanus;
-			Console.WriteLine("Kas sul juba oli sunnipaev sel aastal? (jah/ei)");
-			string vastus = Console.ReadLine(); 
-			if (vastus == "jah")
-			{
-				Console.WriteLine($"Ahhaa, oled sündinud {synniaasta}'l aastal! ");
-			}
-			else if (vastus == "ei")
-			{
-				Console.WriteLine($"Ahhaa, oled sündinud {synniaasta - 1}'l aastal! ");
-			}
-			else
-			{
-				Console.WriteLine("Sa ei tea millal su synna on? :'(");
-			}
+				/*	-= Ülesanne 1 =-	*/
+
+				//1. kirjuta programm, mis
+				//omab täisarvu muutujat vanuse jaoks
+				//küsib kasutajalt tema vanust. int.Parse() üritab muuta andmeid tekstist arvuks
+				//omab täisarvu muutujat aastaarvu jaoks
+				//küsib kasutajalt praegust kalendriaastat. int.Parse() üritab muuta andmeid tekstist arvuks
+				//omab muutujat sünniaasta jaoks, siia sisse omimstatakse tehte tulemus, kus aastaarvust lahutatakse vanus
+
+
+				//int vanus = 0;
+				//Console.WriteLine("Kui vana sa oled?");
+				//vanus = int.Parse(Console.ReadLine());
+				//int aasta = 0;
+				//Console.WriteLine("Mis aasta hetkel on?");
+				//aasta = int.Parse(Console.ReadLine());
+				//int synniaasta = aasta - vanus;
+				//Console.WriteLine("Kas sul juba oli sunnipaev sel aastal? (jah/ei)");
+				//string vastus = Console.ReadLine(); 
+				//if (vastus == "jah")
+				//{
+				//	Console.WriteLine($"Ahhaa, oled sündinud {synniaasta}'l aastal! ");
+				//}
+				//else if (vastus == "ei")
+				//{
+				//	Console.WriteLine($"Ahhaa, oled sündinud {synniaasta - 1}'l aastal! ");
+				//}
+				//else
+				//{
+				//	Console.WriteLine("Sa ei tea millal su synna on? :'(");
+				//}
+
+
+				/*	-= Ülesanne 2 =-	*/
+
+				//2. kirjuta programm, mis
+				//küsib kasutajalt tema nime
+				//hoiab seda nime sõne-tüüpi muutujas
+				//kuvab välja tervituse, liites juurde inimese nime
+
+
+				//Console.WriteLine("Mis su nimi on?");
+				//string nimi = Console.ReadLine();
+				//Console.WriteLine($"No tsau, {nimi}, oleme tuttavad!");
+
+
+				/*	-= Ülesanne 3 =-	*/
+
+				//3. kirjuta programm, mis
+				//omab muutujat kommikaal
+				//omab muutujat sõpradearv
+				//küsib kasutajalt mitu grammi kommi tal on
+				//muutujasse kommikaal pannakse kasutaja sisestatud arv
+				//küsib kasutajalt mitme inimese jaoks ta kommi ostis
+				//ootab kuni kasutaja sisestab arvu, mis omistatakse muutujasse sõpradearv
+				//kuvatakse välja tehe kus kommikaal jagatakse sõpradearvuga
+
+
+
+
+
+			
+
+
+
+
+
+
+
+
 		}
 	}
 }
